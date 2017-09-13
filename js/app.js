fcda777a0ceaa75f3d2bc21f4aa0db5cf5cadd0b
Aviral Garg <aviral@alumni.ubc.ca>
2017-09-13 15:53:03 -0700

    Assignment-1 final clean up

--- js/app.js
+++ js/app.js
@@ -8,20 +8,28 @@
  * TODO list:
  * 1. injecting DOM elements with javascript
  *  */
+
+
 var cart = {};
 var product = {};
 
-var element = document.getElementsByClassName("addToCart");
+/* Information for all the products in the store */
+var productInfo = [{"name": "Box1", "cost": "$10", "imgURI": "images/Box1_$10.png"},
+    {"name": "Box2", "cost": "$5", "imgURI": "images/Box2_$5.png"}];
 
-element[0].addEventListener("click", function () {
-    var selectedProduct = "bow1";
-    // find the name of the selected product
-    console.log(element.parentElement.nodeName);
-    addToCart(selectedProduct);
+/* HTML injection */
+var addToCartButtons = document.getElementsByClassName("addToCart");
+var html = "<a> Google </a>";
+document.getElementById("container").innerHTML = html;
+
+// TODO: for (button in addToCartButtons): something like that
+// TODO-continued: basically add this addEventListner for each element in the addToCartButtons array
+addToCartButtons[0].addEventListener("click", function () {
+    addToCart(productInfo[0].name);
 });
 
 function addToCart(productName) {
-    console.log("addToCart " + productName);
+    console.log("Adding " + productName + " to cart");
 }
 
 function removeFromCart(productName) {
