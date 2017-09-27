10635ccbb43168bda12bd04c2675a040d21a9628
Atif Mahmud <atifmahmud95@gmail.com>
2017-09-26 23:33:21 -0700

    DOM Manipualtion Fails

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+ * JavaScript File for CPEN400A Assignment 2
+ *
+ * Author: Atif Mahmud
+ *
+ * DOM elements will be addded via this script in addition to adding responsiveness
+ *
+ */
+
+ // Array holding the menu items 
+ var navArray = ["All Items", "Books", "Clothing", "Tech", "Gifts", "Stationary", "Supplies"];
+
+
+ // Function to create the list from the array
+ function makeUL(array){
+
+ 	var list = document.create('ul');
+
+ 	for(var i =0; i < array.length(); i++){
+
+ 		// This creates the item
+ 		var item = document.createElement('li');
+ 		
+    	// This sets the contents
+ 		item.appendChild(document.createTextNode(array[i]));
+ 		
+ 		// This adds it to the list
+ 		list.appendChild(item);
+
+ 	}
+
+ 	return list;
+
+ }
+ document.getElementById("navigationMenu").appendChild(makeUL(navArray));
