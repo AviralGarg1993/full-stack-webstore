d9587c39d5b4c05e44682924689e0c89215d513f
Atif Mahmud <atifmahmud95@gmail.com>
2017-09-27 11:25:02 -0700

    More Changes

--- script.js
+++ script.js
@@ -10,27 +10,25 @@
  // Array holding the menu items 
  var navArray = ["All Items", "Books", "Clothing", "Tech", "Gifts", "Stationary", "Supplies"];
 
-
  // Function to create the list from the array
  function makeUL(){
- 	console.log("a");
-
  	var list = document.createElement('ul');
 
  	for(var i =0; i < navArray.length; i++){
-
  		// This creates the item
  		var item = document.createElement('button');
- 		
     	// This sets the contents
  		item.innerHTML += navArray[i];
- 		
  		// This adds it to the list
  		list.appendChild(item);
-
- 		console.log(list);
-
  	}
-
  	document.getElementById("navigationMenu").appendChild(list);
+ }
+
+ var products = [];
+
+
+ //Can I use a table if it is scaleable from this point of view? Like easily manageable?
+ function makeTable(){
+
  }
