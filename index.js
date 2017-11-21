644820c2e61d9a4258ba0aaa646b748e1ff0cda7
Aviral Garg <contact@aviralgarg.com>
2017-11-21 01:29:13 -0800

    Task 3 finished
    + [index.js] updated server-side endpoint for '/products' to return
        price-filtered products from the database
    + [style.css] added hidden class for hiding filtered products
    + [client.js]
        * added ajax request to get price-filtered products
        * added functionality to hide price-filtered products
    + [index.html] added division to get price-filters in <input> nodes

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.json());
 /***** TA's random number code
-function getRandomInt(min, max) {
+ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
  */
 
 
 /* Products object removed, because we need to retrieve from MongoDB, as per Piazza post @301 and @243 */
-var products = [];
+//var products = [];
 
 app.use(express.static(__dirname));
 
@@ -30,59 +30,10 @@ app.get("/navMenuList", function (request, response) {
 });
 
 
-/* This is what we use to get products */
-app.get('/products', function (request, response) {
-
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(err, null);
-
-        var cursor = db.collection('products').find();
-        cursor.each(function (err, doc) {
-            assert.equal(err, null);
-            if (doc !== null) {
-                console.log(doc);
-                products.push(doc);
-            }
-        });
-
-        db.close();
-    });
-
-    response.send(products);
-
-    /*************** TA's code for random number stuff************
-     var option = getRandomInt(0, 5);
-
-     if (option < 4) {
-
-        response.json(products);
-
-        /!********** Code to retrieve data from the MongoDB ***********!/
-
-        MongoClient.connect(url, function (err, db) {
-            assert.equal(err, null);
-
-            var cursor = db.collection('products').find();
-            cursor.each(function (err, doc) {
-                assert.equal(err, null);
-                if (doc !== null) {
-                    console.log(doc);
-                    products.push(doc);
-                }
-            });
-
-            db.close();
-        });
-
-    } else if (option === 4) {
-        response.status(500).send("An error occurred, please try again");
-    }*/
-
-});
 
 app.get('/products/:productKey', function (request, response) {
+
+    console.log('**********************************');
 
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -105,6 +56,71 @@ app.get('/products/:productKey', function (request, response) {
   } else if (option === 4) {
     response.status(500).send("An error occurred, please try again");
   }*/
+});
+
+
+/*
+* To price-filter products send GET request with URL following the syntax below:
+* '/products?minPrice=[lower-limit-number]&maxPrice=[upper-limit-number]'
+*
+* */
+
+app.get('/products', function (request, response) {
+
+    var minPrice = -1, maxPrice = Number.MAX_VALUE;
+    var priceFilters = request.query;
+
+    try {
+
+        if (JSON.stringify(priceFilters) === '{}') {
+            console.log('+ No price filters');
+        } else {
+            // check for invalid price filters request
+            assert.notEqual(priceFilters.minPrice, undefined, 'invalidPriceFilters');
+            assert.notEqual(priceFilters.maxPrice, undefined, 'invalidPriceFilters');
+            minPrice = parseFloat(priceFilters.minPrice);
+            maxPrice = parseFloat(priceFilters.maxPrice);
+            assert.notEqual(isNaN(minPrice) || minPrice <= 0, true, 'invalidPriceFilters');
+            assert.notEqual(isNaN(maxPrice) || maxPrice > Number.MAX_VALUE, true, 'invalidPriceFilters');
+            assert.notEqual(minPrice > maxPrice, true, 'invalidPriceFilters');
+
+            // received price filters are valid
+            console.log('+ Valid Price Filters (min: ' + minPrice + ' max: ' + maxPrice + ')');
+        }
+
+        response.header("Access-Control-Allow-Origin", "*");
+        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        MongoClient.connect(url, function (err, db) {
+            var products = [];
+            assert.equal(err, null);
+            var cursor = db.collection('products').find({price: {$gte: minPrice, $lte: maxPrice}});
+            cursor.forEach(function (doc) {
+                if (doc !== null) {
+                    console.log(doc.name);
+                    products.push(doc);
+                }
+            }, function (err) {
+                if (err === null)
+                    response.json(products);
+            });
+
+            db.close();
+        });
+
+
+    } catch (err) {
+        console.error(err);
+        if (err.message === 'invalidPriceFilters') {
+            response.status(400);
+            err.message = '- Invalid Price Filters (minPrice= ' + priceFilters.minPrice + ' maxPrice= ' + priceFilters.maxPrice + ')';
+        } else {
+            response.status(500);
+        }
+        response.send(err.message);
+
+    }
+
 });
 
 
